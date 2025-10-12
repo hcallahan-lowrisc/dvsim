37119b8567516352499e37de4a6bfb5f37e4237b
Harry Callahan <hcallahan@lowrisc.org>
2025-10-12 16:18:43 +0100

    My non-conventional commit with inconsequential changes
    Pushed to check that no release is cut

--- scripts/license_check.py
+++ scripts/license_check.py
@@ -3,7 +3,7 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Check all files for license header."""
+"""Check files for license header."""
 
 import subprocess
 import sys
